6c8872816d047a49ba533de5fbb2c4043ad05c36
Akien Maciain <akienm@gmail.com>
2024-06-14 16:10:18 -0600

    fixed the thing that starts selenium

--- bin/startselenium.bat
+++ bin/startselenium.bat
@@ -1,1 +1,1 @@
-start java -jar "C:\akien\dev\src\swadl\bin\selenium-server-4.10.0\selenium-server-4.10.0.jar" standalone
+start java -jar "C:\Users\akien\OneDrive\Documents\dev\src\swadl\bin\selenium-server-4.10.0.jar" standalone
