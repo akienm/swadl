958f414b38c440740cb005a840962914f3985c38
marniehackbarth <marniehackbarth@gmail.com>
2024-07-05 16:28:27 -0700

    added first link

--- Project/flows/google_search_flow.py
+++ Project/flows/google_search_flow.py
@@ -24,3 +24,7 @@ class GoogleFlows(SWADLBaseFlow):
         # Purpose: Validate that the search_key provided is in the result headers somewhere
         # Keys: Emits matching title list in Project.flows.google_search_constants.SEARCH_RESULT_TITLES_LIST
         self.google_results_page.get_matching_results()
+
+    def validate_background_gui(self):
+        #purpose:
+        self.google_search_page.validate_background_gui()
--- Project/page_sections/google_search_section.py
+++ Project/page_sections/google_search_section.py
@@ -24,8 +24,18 @@ class GoogleSearchSection(SWADLPageSection):
             validation={VALIDATE_VISIBLE: True},
         )
 
+        self.about_link = SWADLControl(
+            name="about_link",
+            parent=self,
+            selector='.MV3Tnb',
+            validation={VALIDATE_VISIBLE: True},
+        )
         # used by self.validate_loaded()
         self.validate_loaded_queue = [self.search_box]
+        self.validate_all = [
+            self.search_box,
+            self.about_link
+        ]
 
     def do_search(self):
         # Purpose: loads page if it's not loaded
@@ -35,3 +45,8 @@ class GoogleSearchSection(SWADLPageSection):
         self.load_page()
         self.search_box.send_keys(value=self.test_data[SEARCH_KEY])
         self.search_box.submit()
+
+    def validate_background_gui(self):
+        # purpose:
+        self.load_page()
+        self.validate_controls(controls=self.validate_all, validation=VALIDATE_VISIBLE)
