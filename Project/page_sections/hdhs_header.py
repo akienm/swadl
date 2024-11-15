a0c60bdaa58f0c6ec2b05f48288438860b3373b6
marniehackbarth <marniehackbarth@gmail.com>
2024-11-15 11:39:17 -0800

    code part 1 for hdhs

---
+++ Project/page_sections/hdhs_header.py
@@ -0,0 +1,121 @@
+
+
+from SWADL.engine.swadl_constants import VALIDATE_VISIBLE
+from SWADL.engine.swadl_control import SWADLControl
+from SWADL.engine.swadl_base_section import SWADLPageSection
+
+
+class HDHSHeader(SWADLPageSection):
+    # Purpose: hdhs navigation
+
+    def __init__(self, name="HDHSHeader", **kwargs):
+        # Purpose: describe the page
+        super().__init__(name=name, **kwargs)
+        self.url = "https://www.highdeserthumane.org/"
+
+        self.banner = SWADLControl(
+            is_text="High Desert Humane Society",
+            name="HDHSBanner",
+            parent=self,
+            selector='span',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.banner = SWADLControl(
+            is_text="Adoption Policies and Fees",
+            name="HDHSBanner",
+            parent=self,
+            selector='.dir-ltr',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.banner = SWADLControl(
+            is_text="H.A.L.T. Spay Neuter",
+            name="HDHSBanner",
+            parent=self,
+            selector='.label-text-bold',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.banner = SWADLControl(
+            is_text="More",
+            name="Adoption Policies and Fees",
+            parent=self,
+            selector='.dir-ltr',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.banner = SWADLControl(
+            is_text="Monthly Rabies Vaccination Clinics Information",
+            name="HDHSBanner",
+            parent=self,
+            selector='.label-text-bold',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.banner = SWADLControl(
+            is_text="About Us",
+            name="HDHSBanner",
+            parent=self,
+            selector='.dir-ltr',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.more = SWADLControl(
+            is_text="More",
+            name="HeaderMore",
+            parent=self,
+            selector='.dir-ltr',
+            validation={VALIDATE_VISIBLE: True},
+        )
+
+        self.contact = SWADLControl(
+            is_text="Services",
+            name="HeaderMore",
+            parent=self,
+            selector='div.page-title.text-overflow',
+            validation={VALIDATE_VISIBLE: False},
+        )
+
+        self.contact = SWADLControl(
+            is_text="Donations",
+            name="HeaderMore",
+            parent=self,
+            selector='div.page-title.text-overflow',
+            validation={VALIDATE_VISIBLE: False},
+        )
+
+        self.contact = SWADLControl(
+            is_text="Volunteer",
+            name="HeaderMore",
+            parent=self,
+            selector='div.page-title.text-overflow',
+            validation={VALIDATE_VISIBLE: False},
+        )
+
+        self.contact = SWADLControl(
+            is_text="Education",
+            name="HeaderMore",
+            parent=self,
+            selector='div.page-title.text-overflow',
+            validation={VALIDATE_VISIBLE: False},
+        )
+        self.contact = SWADLControl(
+            is_text="News and Videos",
+            name="HeaderMore",
+            parent=self,
+            selector='div.page-title.text-overflow',
+            validation={VALIDATE_VISIBLE: False},
+        )
+
+        # used by self.validate_loaded()
+        self.validate_loaded_queue = [self.banner, self.more]
+
+    def do_search(self):
+        # Purpose: loads page if it's not loaded
+        # Keys: SEARCH_KEY
+        # Emits: "GoogleSearchSection loaded ok": True" = page load validated
+
+        self.load_page()
+        self.search_box.set_value(value=self.test_data[SEARCH_KEY])
+        self.search_box.submit()
